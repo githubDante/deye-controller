3d447c3343282ad491e16e10f195436a673b9577
guthubDante <github@dante.tk>
2023-03-06 21:20:25 +0200

    Use the Grid / Gen features
    Updated examples
    Updated docstrings

--- deye_controller/sell_programmer.py
+++ deye_controller/sell_programmer.py
@@ -1,7 +1,8 @@
 from pysolarmanv5 import PySolarmanV5, V5FrameError
-from .modbus.protocol import (HoldingRegisters, WriteableRegisters, Register, WritableRegister,
-                            SellTimePoint, IntType, FloatType
+from .modbus.protocol import (HoldingRegisters, WritableRegisters, Register, WritableRegister,
+                              SellTimePoint, IntType, FloatType, ChargeTimePoint
                               )
+from .modbus.enums import ChargeGridGen
 from typing import Dict
 
 
@@ -9,31 +10,44 @@ class SellProg:
 
     """ Combined elements of an inverter program """
 
-    def __init__(self, index: int, time: int, power: int, voltage: int, capacity: int):
+    def __init__(self, index: int, time: int, power: int, voltage: int, capacity: int, grid_gen: int):
+        """
+        Holder for all elements of an inverter program
+
+        :param index: Index of the program [0-6]
+        :param time: Start time as string 'HH:MM'
+        :param power: Max discharge power (charge seems unaffected by this)
+        :param voltage: Min/Max battery voltage (if the control is by voltage)
+        :param capacity: SOC percents
+        :param grid_gen: Actual value of the Time Point X charge enable register (registers from 172 to 177)
+        """
+
         self.index = index
         self.time = SellTimePoint(148 + self.index, '')
         self.power = IntType(154 + self.index, '', suffix='W')
         self.voltage = FloatType(160 + self.index, '', 100, suffix='V')
         self.soc = IntType(166 + self.index, '', suffix='%')
+        self.grid_gen = ChargeTimePoint(172 + self.index, '')
 
         self.time.value = time
         self.power.value = power
         self.voltage.value = voltage
         self.soc.value = capacity
+        self.grid_gen.value = grid_gen
 
     def set_time(self, new_t: str):
         """ Set the start time for this program """
-        writable = [WriteableRegisters.SellModeT1, WriteableRegisters.SellModeT2, WriteableRegisters.SellModeT3,
-                    WriteableRegisters.SellModeT4, WriteableRegisters.SellModeT5, WriteableRegisters.SellModeT6]
+        writable = [WritableRegisters.SellModeT1, WritableRegisters.SellModeT2, WritableRegisters.SellModeT3,
+                    WritableRegisters.SellModeT4, WritableRegisters.SellModeT5, WritableRegisters.SellModeT6]
         reg = [x for x in writable if self.time.address == x.address][0]
         reg.set(new_t)
         self.time.value = reg.modbus_value
 
     def set_power(self, watts: int):
         """ Set the MAX power for this program """
-        writable = [WriteableRegisters.SellModeWatts1, WriteableRegisters.SellModeWatts2,
-                    WriteableRegisters.SellModeWatts3, WriteableRegisters.SellModeWatts4,
-                    WriteableRegisters.SellModeWatts5, WriteableRegisters.SellModeWatts6
+        writable = [WritableRegisters.SellModeWatts1, WritableRegisters.SellModeWatts2,
+                    WritableRegisters.SellModeWatts3, WritableRegisters.SellModeWatts4,
+                    WritableRegisters.SellModeWatts5, WritableRegisters.SellModeWatts6
                     ]
         reg = [x for x in writable if self.power.address == x.address][0]
         reg.set(watts)
@@ -41,11 +55,66 @@ class SellProg:
 
     def set_soc(self, soc_pct: int):
         """ Set the SOC value for this program """
-        writable = [WriteableRegisters.SellModeSOC1, WriteableRegisters.SellModeSOC2, WriteableRegisters.SellModeSOC3,
-                    WriteableRegisters.SellModeSOC4, WriteableRegisters.SellModeSOC5, WriteableRegisters.SellModeSOC6]
+        writable = [WritableRegisters.SellModeSOC1, WritableRegisters.SellModeSOC2, WritableRegisters.SellModeSOC3,
+                    WritableRegisters.SellModeSOC4, WritableRegisters.SellModeSOC5, WritableRegisters.SellModeSOC6]
         reg = [x for x in writable if self.soc.address == x.address][0]
         reg.set(soc_pct)
         self.soc.value = reg.modbus_value
+
+    def activate_grid_charge(self):
+        """ Set the GRID charge flag """
+        writable = [WritableRegisters.ChargeGridGen1, WritableRegisters.ChargeGridGen2,
+                    WritableRegisters.ChargeGridGen3, WritableRegisters.ChargeGridGen4,
+                    WritableRegisters.ChargeGridGen5, WritableRegisters.ChargeGridGen6]
+        reg = [x for x in writable if self.grid_gen.address == x.address][0]
+
+        if self.grid_gen.value == ChargeGridGen.GenEnabled:
+            reg.set(ChargeGridGen.GridGenEnabled)
+            self.grid_gen.value = reg.modbus_value
+        elif self.grid_gen.value == ChargeGridGen.GridGenDisabled:
+            reg.set(ChargeGridGen.GridEnabled)
+            self.grid_gen.value = reg.modbus_value
+
+    def deactivate_grid_charge(self):
+        """ Unset the GRID charge flag """
+        writable = [WritableRegisters.ChargeGridGen1, WritableRegisters.ChargeGridGen2,
+                    WritableRegisters.ChargeGridGen3, WritableRegisters.ChargeGridGen4,
+                    WritableRegisters.ChargeGridGen5, WritableRegisters.ChargeGridGen6]
+        reg = [x for x in writable if self.grid_gen.address == x.address][0]
+        if self.grid_gen.value == ChargeGridGen.GridEnabled:
+            reg.set(ChargeGridGen.GridGenDisabled)
+            self.grid_gen.value = reg.modbus_value
+        elif self.grid_gen.value == ChargeGridGen.GridGenEnabled:
+            reg.set(ChargeGridGen.GenEnabled)
+            self.grid_gen.value = reg.modbus_value
+
+    def activate_generator_charge(self):
+        """ Set the GEN charge flag """
+        writable = [WritableRegisters.ChargeGridGen1, WritableRegisters.ChargeGridGen2,
+                    WritableRegisters.ChargeGridGen3, WritableRegisters.ChargeGridGen4,
+                    WritableRegisters.ChargeGridGen5, WritableRegisters.ChargeGridGen6]
+        reg = [x for x in writable if self.grid_gen.address == x.address][0]
+
+        if self.grid_gen.value == ChargeGridGen.GridEnabled:
+            reg.set(ChargeGridGen.GridGenEnabled)
+            self.grid_gen.value = reg.modbus_value
+        elif self.grid_gen.value == ChargeGridGen.GridGenDisabled:
+            reg.set(ChargeGridGen.GenEnabled)
+            self.grid_gen.value = reg.modbus_value
+
+    def deactivate_generator_charge(self):
+        """ Unset the GEN charge flag """
+        writable = [WritableRegisters.ChargeGridGen1, WritableRegisters.ChargeGridGen2,
+                    WritableRegisters.ChargeGridGen3, WritableRegisters.ChargeGridGen4,
+                    WritableRegisters.ChargeGridGen5, WritableRegisters.ChargeGridGen6]
+        reg = [x for x in writable if self.grid_gen.address == x.address][0]
+
+        if self.grid_gen.value == ChargeGridGen.GenEnabled:
+            reg.set(ChargeGridGen.GridGenDisabled)
+            self.grid_gen.value = reg.modbus_value
+        if self.grid_gen.value == ChargeGridGen.GridGenEnabled:
+            reg.set(ChargeGridGen.GridEnabled)
+            self.grid_gen.value = reg.modbus_value
 
     def __repr__(self):
         return f'<DEYE-Prog{self.index}> {self.time.format()} - P: {self.power.format()} / SOC: {self.soc.format()}'
@@ -67,33 +136,33 @@ class SellProgrammer:
         >>> from deye_controller.sell_programmer import SellProgrammer
         >>> prog = SellProgrammer('192.168.1.108', 2799999999)
         >>> prog.show_as_screen()
-        _____________________________________
-        |      Time     |   Pwr    |  SOC % |
-        | 00:00 | 18:00 |     3500 |   100% |
-        | 18:00 | 19:00 |     3500 |    30% |
-        | 19:00 | 20:00 |     3500 |    30% |
-        | 20:00 | 21:00 |     3500 |    30% |
-        | 21:00 | 22:00 |     3500 |    30% |
-        | 22:00 | 00:00 |     3500 |    30% |
-        -------------------------------------
-        >>> prog.update_program(3, start_t='20:30', power=2500, soc=35)
+        ____________________________________________________
+        | Grid  |  Gen  |      Time     |   Pwr    |  SOC % |
+        |       |       | 00:00 | 03:00 |     3500 |   100% |
+        |       |       | 03:00 | 04:00 |     3500 |    30% |
+        |       |       | 04:00 | 05:00 |     3500 |    30% |
+        |       |       | 05:00 | 10:00 |     3500 |    30% |
+        |       |       | 10:00 | 23:00 |     3500 |   100% |
+        |       |       | 23:00 | 00:00 |     3500 |    30% |
+        ----------------------------------------------------
+        >>> prog.update_program(3, start_t='6:30', power=2500, soc=35, grid_ch=True)
         Program updated
         >>> prog.show_as_screen()  # For visual confirmation of the settings
-        _____________________________________
-        |      Time     |   Pwr    |  SOC % |
-        | 00:00 | 18:00 |     3500 |   100% |
-        | 18:00 | 19:00 |     3500 |    30% |
-        | 19:00 | 20:30 |     3500 |    30% |
-        | 20:30 | 21:00 |     2500 |    35% |
-        | 21:00 | 22:00 |     3500 |    30% |
-        | 22:00 | 00:00 |     3500 |    30% |
-        -------------------------------------
-        >>> prog.upload_settings()  # In order to upload the settigs to the inverter
+        ____________________________________________________
+        | Grid  |  Gen  |      Time     |   Pwr    |  SOC % |
+        |       |       | 00:00 | 03:00 |     3500 |   100% |
+        |       |       | 03:00 | 04:00 |     3500 |    30% |
+        |       |       | 04:00 | 06:30 |     3500 |    30% |
+        |   ✓   |       | 06:30 | 10:00 |     2500 |    35% |
+        |       |       | 10:00 | 23:00 |     3500 |   100% |
+        |       |       | 23:00 | 00:00 |     3500 |    30% |
+        ----------------------------------------------------
+        >>> prog.upload_settings()  # In order to upload the settings to the inverter
     """
 
     def __init__(self, ip_address: str, serial: int):
         self.prog_start = 148  # Sell settings initial address (inclusive)
-        self.prog_end = 172  # Last sell settings address (exclusive)
+        self.prog_end = 178  # Last sell settings address (exclusive)
         self.reg_qty = self.prog_end - self.prog_start
         self.programs: Dict[int, SellProg] = {}
         self.ip_addr = ip_address
@@ -113,29 +182,31 @@ class SellProgrammer:
         for i in range(6):
             self.programs[i] = SellProg(i, *reg_values[i::6])
 
-    def upload_settings(self):
+    def upload_settings(self) -> None:
         """
-
-        :return:
-        :rtype:
+        Upload the settings back to the inverter
         """
         """ Register sequence: time, power, voltage, capacity """
         values_t = []
         values_p = []
         values_v = []
         values_c = []
+        values_g = []
+
         for i in range(6):
             p = self.programs.get(i)
-            values_t.append(p.time.value)
-            values_p.append(p.power.value)
-            values_v.append(p.voltage.value)
-            values_c.append(p.soc.value)
+            values_t.append(p.time.value)  # Start time
+            values_p.append(p.power.value)  # Power control
+            values_v.append(p.voltage.value)  # Voltage control
+            values_c.append(p.soc.value)  # Capacity
+            values_g.append(p.grid_gen.value)  # Grid / Gen
 
-        values = values_t + values_p + values_v + values_c
+        values = values_t + values_p + values_v + values_c + values_g
         ret_values = self.modbus.write_multiple_holding_registers(self.prog_start, values)
         """ It's possible to check the values returned after write """
 
-    def update_program(self, index: int, start_t: str = None, soc: int = None, power: int = None):
+    def update_program(self, index: int, start_t: str = None, soc: int = None, power: int = None,
+                       grid_ch: bool = None, gen_ch: bool = None):
         """
         SellTime program update
 
@@ -143,42 +214,28 @@ class SellProgrammer:
         :param start_t: Start time of the program
         :param soc: State of charge (in percents)
         :param power: Discharge rate per hour (in watts)
+        :param grid_ch: Set to True / False in order top change the Grid checkbox of the respective program
+        :param gen_ch: Set to True / False in order top change the Gen checkbox of the respective program
         :return:
         """
         program = self.programs.get(index)
         if program:
             if start_t:
-                self._set_time_with_check(index, start_t)
+                program.set_time(start_t)
             if soc:
                 program.set_soc(soc)
             if power:
                 program.set_power(power)
 
-    def _set_time_with_check(self, index, t: str):
-        next_idx = index + 1
-        prev_idx = index - 1
-        if index == 5:
-            next_idx = 0
-        if index == 0:
-            prev_idx = 5
+            if grid_ch and grid_ch is True:
+                program.activate_grid_charge()
+            elif grid_ch is False:
+                program.deactivate_grid_charge()
 
-        p = self.programs.get(index)
-        old_t = p.time.value
-        p.set_time(t)
-
-        if self.programs[prev_idx].time.value >= p.time.value and prev_idx != 5:
-            print('Invalid time spec '
-                  f'The time of the program must be '
-                  f'greater than the previous one: {self.programs[prev_idx].time.format()}')
-            p.time.value = old_t
-        elif self.programs[next_idx].time.value <= p.time.value and next_idx != 0:
-            print('Invalid time spec '
-                  f'The time of the program must be '
-                  f'lower than the next one: {self.programs[next_idx].time.format()}')
-            p.time.value = old_t
-
-        else:
-            print('Program updated')
+            if gen_ch and gen_ch is True:
+                program.activate_generator_charge()
+            elif gen_ch is False:
+                program.deactivate_generator_charge()
 
     def show_as_screen(self):
         """
@@ -186,8 +243,8 @@ class SellProgrammer:
         :return:
         :rtype:
         """
-        header = '_' * 37
-        header += '\n|  {:^12s} | {:^8s} | {:^5s}% |'.format('Time', 'Pwr', 'SOC')
+        header = '_' * 52
+        header += '\n| {:^5} | {:^5} |  {:^12s} | {:^8s} | {:^5s}% |'.format('Grid', 'Gen', 'Time', 'Pwr', 'SOC')
         print(header)
         for i in range(6):
             p1 = self.programs.get(i)
@@ -195,7 +252,12 @@ class SellProgrammer:
                 p2 = self.programs.get(0)
             else:
                 p2 = self.programs.get(i+1)
-            table = '| {:>5s} | {:>5s} | {:>8} | {:>5}% |'.format(p1.time.format(), p2.time.format(),
+            f_grid = '✓' if p1.grid_gen.value in [ChargeGridGen.GridEnabled, ChargeGridGen.GridGenEnabled] else ''
+            f_gen = '✓' if p1.grid_gen.value in [ChargeGridGen.GenEnabled, ChargeGridGen.GridGenEnabled] else ''
+            """ Grid / Gen selection  """
+
+            table = '| {:^5s} | {:^5s} | {:>5s} | {:>5s} | {:>8} | {:>5}% |'.format( f_grid, f_gen,
+                                                                   p1.time.format(), p2.time.format(),
                                                                    p1.power.format(), p1.soc.format())
             print(table)
-        print('-' * 37)
+        print('-' * 52)
